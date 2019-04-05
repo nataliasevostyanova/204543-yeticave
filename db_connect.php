c19ef6df22462871d9e91e44ef9e21ea6f3e255d
Natalia <sevostyanova@gmail.com>
2019-04-06 02:16:38 +0300

    готовы главная страница + переход на стр. лота

---
+++ db_connect.php
@@ -0,0 +1,16 @@
+<?php
+
+$link = mysqli_init();
+mysqli_options($link, MYSQLI_OPT_INT_AND_FLOAT_NATIVE, 1);
+mysqli_real_connect($link, "localhost", "root", "", "Yeticave_pro");
+mysqli_set_charset($link, "utf8");
+/*проверка соединения с БД*/
+/*
+if ($link == false) {
+print("Ошибка подключения: " . mysqli_connect_error());
+}
+else {
+print("Соединение установлено;");
+}*/
+
+?>
---
+++ lot.php
@@ -0,0 +1,42 @@
+<?php
+
+require_once('data_db.php');       /*подключаем БД и данные запросов*/
+require_once('functions_01.php');  /*подключаем функции*/
+
+if(isset($_GET['id_lot'])){
+  $lot_id = $_GET['id_lot'];
+  /*print('id_lot = '.$lot_id);*/
+ 
+ /*запрос на информацию о лоте по его id -- получаем массив $lot_info*/
+  
+  $sql =  "SELECT name, description, img_url, cat_id, start_price, category FROM lots l JOIN cats c ON l.cat_id=c.id WHERE l.id_lot = $lot_id";
+
+   $result = mysqli_query($link, $sql);
+            if(!$result){
+              $error = mysqli_error($link);
+              print("Ошибка MySQL: ". $error);
+            }
+   $row = mysqli_fetch_all($result, MYSQLI_ASSOC);
+   foreach($row as $item)
+  {
+    $lot_inf = $item;
+  }
+/*
+   print('<pre>');
+   var_dump($lot_inf);
+   print('</pre>');*/
+}
+  else{
+  header( 'Location: pages/404.html' /*"HTTP/1.1 404 Not Found"*/); 
+
+}
+
+
+$lot_inform = include_template('lot_content.php',['lot_id'=> $lot_id, 'lot_inf' =>$lot_inf]); /*lot information*/
+$lot_title = $lot_inf['name']; /* название лота */
+
+$layout_lot = include_template('layout_lot.php', ['lot_info' => $lot_inform, 'lot_title' => $lot_title]); 
+
+print($layout_lot);
+?> 
+
