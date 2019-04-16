57b73e57a06916abf85596568f7624400cf062fb
Natalia <sevostyanova@gmail.com>
2019-04-16 14:57:20 +0300

    переписаа через mysqli_connect

--- db_connect.php
+++ db_connect.php
@@ -1,12 +1,16 @@
 <?php
 
-$link = mysqli_init();
+/*$link = mysqli_init();
 mysqli_options($link, MYSQLI_OPT_INT_AND_FLOAT_NATIVE, 1);
 mysqli_real_connect($link, "localhost", "root", "", "Yeticave_pro");
+mysqli_set_charset($link, "utf8");*/
+
+
+$link = mysqli_connect("localhost", "root", "", "Yeticave_pro");
 mysqli_set_charset($link, "utf8");
+
 /*проверка соединения с БД*/
-/*
-if ($link == false) {
+/*if ($link == false) {
 print("Ошибка подключения: " . mysqli_connect_error());
 }
 else {
